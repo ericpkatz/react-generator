8d8ed01108400d29eb41f738e284c6f90c8ec1d2
Eric P. Katz <ericpkatz@gmail.com>
2017-10-31 07:43:56 -0400

    commit

--- scripts/AppGenerator.js
+++ scripts/AppGenerator.js
@@ -203,6 +203,7 @@ class AppGenerator{
 <html> 
   <head>
     ${AppGenerator.scripts()}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
     <div id='root'></div>
